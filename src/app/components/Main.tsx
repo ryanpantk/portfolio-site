5af549f786bf5a1c4b01786ae93cc4b01e9fa987
Ryan <ryanpantk@gmail.com>
2023-12-24 01:18:04 +0800

    feat: change color scheme

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -6,9 +6,9 @@ const Main = () => {
 		<div id="main" className="bg">
 			<div className="h-screen flex justify-start items-center px-28">
                 <div className='mr-auto pl-28'>
-                    <p className="font-mukta text-4xl text-gray-800 font-semibold tracking-wide">Hello! I am <span className='text-slate-200'>Ryan Pan</span></p>
-                    <p className="font-mukta text-2xl text-gray-800 font-semibold py-2 tracking-wide">I&apos;m an aspiring <span className='text-slate-200'>Software Engineer</span></p>
-                    <p className="font-mukta text-sm text-gray-800 font-semibold py-3 tracking-wide">Currently a student @ SUTD.</p>
+                    <p className="font-mukta text-5xl text-gray-300 font-semibold tracking-wide">Hello! I am <span className='text-rose-300'>Ryan Pan</span></p>
+                    <p className="font-mukta text-2xl text-gray-300 font-semibold py-2 tracking-wide">I&apos;m an aspiring <span className='text-rose-300 underline'>Software Engineer</span></p>
+                    <p className="font-mukta text-md text-gray-300 font-semibold py-3 tracking-wide">Currently a student @ SUTD.</p>
                 </div>
             <ParisCat/>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import Main from './components/Main'
 import NavBar from './components/NavBar/NavBar'
-import Image from 'next/image';
-import logo from '../../public/self.jpeg';
+import About from './components/About'
 
 export default function Home() {
   return (
     <main>
       <NavBar/>
       <Main/>
-      <div id="about" className="h-screen">
-        {/* <Image src={logo} alt="Logo" /> */}
-      </div>
+      <About/>
+      <div className="h-screen"></div>
     </main>
   )
 }
