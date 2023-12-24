81d8f0452addde11e5eaa25e9c054f57352f19a7
Ryan <ryanpantk@gmail.com>
2023-12-24 20:55:53 +0800

    feat: add progress bar

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,10 +1,14 @@
+"use client";
+
+import { MotionValue } from 'framer-motion'
 import '../../globals.css'
+import { motion, useScroll } from "framer-motion"
 
 
 function Logo(props: {label: string, hrefLink: string}) {
     return (
     <a href={props.hrefLink} className="flex items-center mr-auto">
-        <span className="font-bold font text-2xl ml-3 pl-1 pr-1">{props.label}</span>
+        <span className="font-bold font text-2xl ml-3 pl-1 pr-1 text-rose-300">{props.label}</span>
     </a>
     )
 }
@@ -12,18 +16,21 @@ function Logo(props: {label: string, hrefLink: string}) {
 function NavItem(props: {label: string, hrefLink: string}) {
     return (
     <li>
-        <a href={props.hrefLink} className=" hover:text-black py-3 px-4 text-gray-800 text-sm font-medium rounded-full relative after:bg-black after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
+        <a href={props.hrefLink} className=" hover:text-rose-300 py-3 px-4 text-slate-400 text-sm font-medium rounded-full relative after:bg-rose-300 after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
             {props.label}
         </a>
     </li>
     )
 }
 
-const NavBar = () => {
+const NavBar = (props: {scaleX: MotionValue<any>}) => {
     return (
-        <nav className="fixed w-full backdrop-filter backdrop-blur-sm bg-gray-200 bg-opacity-20 shadow-md z-50 top-0 start-0">
+        <nav className="fixed w-full backdrop-filter backdrop-blur-sm bg-gray-200 bg-opacity-20 shadow-md z-50">
+            {/* Navigation Bar */}
             <div className="flex flex-wrap px-8 py-3">
+                {/* Name Logo */}
                 <Logo label="RP" hrefLink="https://ryanpantk.github.io/portfolio-site/" />
+                {/* Buttons */}
                 <div className="items-center justify-end hidden w-full md:flex md:w-auto md:order-1">
                     <ul className="flex flex-col md:space-x-4 md:flex-row md:mt-0 md:border-0">
                         <NavItem label="Main" hrefLink="#main" />
@@ -34,6 +41,8 @@ const NavBar = () => {
                     </ul>
                 </div>
             </div>
+            {/* Progress Bar */}
+            <motion.div className='progress-bar bg-rose-300 bg-opacity-70 h-1' style={{ scaleX: props.scaleX }} />
         </nav>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+"use client";
+
 import Main from './components/Main'
 import NavBar from './components/NavBar/NavBar'
 import About from './components/About'
+import { useScroll, useSpring } from "framer-motion"
 
 export default function Home() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, { stiffness: 100, damping: 30, restDelta: 0.001 })
   return (
     <main>
-      <NavBar/>
+      <NavBar scaleX={scaleX}/>
       <Main/>
       <About/>
-      <div className="h-screen"></div>
     </main>
   )
 }
