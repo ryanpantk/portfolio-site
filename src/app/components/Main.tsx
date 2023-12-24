e6dd2292a440c0c7945cdd9e9bb74a95ac3cc34d
Ryan <ryanpantk@gmail.com>
2023-12-24 20:58:00 +0800

    style: add underline to main text

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -6,9 +6,9 @@ const Main = () => {
 		<div id="main" className="bg">
 			<div className="h-screen flex justify-start items-center px-28">
                 <div className='mr-auto pl-28'>
-                    <p className="font-mukta text-5xl text-gray-300 font-semibold tracking-wide">Hello! I am <span className='text-rose-300'>Ryan Pan</span></p>
-                    <p className="font-mukta text-2xl text-gray-300 font-semibold py-2 tracking-wide">I&apos;m an aspiring <span className='text-rose-300 underline'>Software Engineer</span></p>
-                    <p className="font-mukta text-md text-gray-300 font-semibold py-3 tracking-wide">Currently a student @ SUTD.</p>
+                    <p className="text-5xl text-gray-300 font-semibold tracking-wide">Hello! I am <span className='text-rose-300'>Ryan Pan</span></p>
+                    <p className="text-2xl text-gray-300 font-semibold py-2 tracking-wide">I&apos;m an aspiring <span className='text-rose-300 underline underline-offset-8'>Software Engineer</span></p>
+                    <p className="text-md text-gray-400 font-semibold py-3 tracking-wide">Currently a student @ SUTD.</p>
                 </div>
             <ParisCat/>
             </div>
